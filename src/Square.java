682c54f725ea3080217163a6fcdb18c883ac948f
Evrim Sahin <evrmsahin@gmail.com>
2019-10-26 17:53:15 +0300

    add all constructor for first iteration

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,25 @@
 public abstract class Square {
+    private String name;
+    private int position;
+
+    public Square (String name, int position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        position = position;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
