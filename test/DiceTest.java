3d246471eff2ec744ad536863391cf6e56ae2aff
Evrim Sahin <evrmsahin@gmail.com>
2019-10-28 19:37:34 +0300

    adding some test cases

---
+++ test/DiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceTest {
+
+    Dice diceTest = new Dice();
+
+    @Test
+    public void getFaces() {
+        int[] diceTestArray = diceTest.getFaces();
+        assertTrue(diceTestArray.length == 2);
+        assertTrue(diceTestArray[0] <= 6 && diceTestArray[0] >= 1);
+        assertTrue(diceTestArray[1] <= 6 && diceTestArray[1] >= 1);
+    }
+
+}
