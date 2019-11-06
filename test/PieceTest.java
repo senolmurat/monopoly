55733c730c0f593a9eab0dcf419126d46336ba68
Murat ŞENOL <murat.senol99@gmail.com>
2019-11-06 16:34:31 +0300

    Player Test class implemented. Piece test class tweaked.

--- test/PieceTest.java
+++ test/PieceTest.java
@@ -7,16 +7,18 @@ public class PieceTest {
 
     Piece testPiece = new Piece();
     Dice testDice = new Dice();
-    Reader testReader = new Reader();
-    Board testBoard = new Board(testReader.getNumberOfTaxSquare(), testReader.getTaxAmount());
+    Board testBoard = new Board(15, 150, 200);
 
 
     int[] diceTestArray = testDice.getFaces();
     int sumOfFaces = diceTestArray[0] + diceTestArray[1];
 
+
     @Test
-    public int move(int sumOfFaces, Board testBoard) {
-        int returnVal = testPiece.move(sumOfFaces, testBoard);
+    public int move(int sumOfFaces_M, Board testBoard_M) {
+        sumOfFaces_M = sumOfFaces;
+        testBoard_M = testBoard;
+        int returnVal = testPiece.move(sumOfFaces_M, testBoard_M);
         assertTrue(testPiece.getPosition() <= 40 && testPiece.getPosition() >= 1);
 
         return returnVal;
