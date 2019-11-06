908b6697c210a7f451829564ee927f6df4a25743
Evrim Sahin <evrmsahin@gmail.com>
2019-11-06 16:38:07 +0300

    Display and Board test classes added.

---
+++ test/DisplayTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class DisplayTest {
+
+    Dice dice = new Dice();
+    Player playerTest;
+    Player playerTest1;
+    Display displayTest = new Display();
+
+    {
+        try {
+            playerTest = new Player(1," Ali ", 2000, dice );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void infoMessageBeforeTossDie() {
+        displayTest.infoMessageBeforeTossDie(playerTest," Tax Square");
+    }
+
+    @Test
+    public void infoMessageAfterTossDie() {
+        displayTest.infoMessageAfterTossDie(playerTest,dice," Tax Square");
+    }
+
+    @Test
+    public void infoBasedOnBalance() {
+    }
+
+    @Test
+    public void selectionSort() {
+    }
+}
