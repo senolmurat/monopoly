0d96e454b5d4682317e61f079ccb888a07d8d533
Murat ŞENOL <murat.senol99@gmail.com>
2019-11-20 15:02:38 +0300

    RE-Commit : Chance Cards are added

---
+++ src/main/java/GameElements/Cards/Chance/Card6.java
@@ -0,0 +1,18 @@
+package GameElements.Cards.Chance;
+
+import GameElements.Cards.Card;
+import Player.Player;
+
+public class Card6 extends Card {
+
+    public Card6(){
+        super.setName("Chance6");
+        super.setDescription("You have won a crossword competition , Collect $100");
+        super.setAltDescription("Which one is the real reward ? , knowledge of puzzle or the money , Mr. Monopoly thinks... ");
+    }
+
+    @Override
+    public void action(Player player) {
+
+    }
+}
