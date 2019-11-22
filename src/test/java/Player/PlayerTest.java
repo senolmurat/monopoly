37c1daed9e5097f29804d7c96db0b27893eea602
Evrim Sahin <evrmsahn@gmail.com>
2019-11-22 22:58:44 +0300

    getUtilitySquaresTest method added

--- src/test/java/Player/PlayerTest.java
+++ src/test/java/Player/PlayerTest.java
@@ -13,12 +13,4 @@ public class PlayerTest {
     Board board = new Board(10, 100,200);
     Dice dice = new Dice();
 
-    @Test
-    public void tossDie() throws Exception {
-        Player player = new Player(1, "Ali", 2000, dice);
-        System.out.print("Faces of Dices " + player.tossDie(dice, isMoving, board));
-        assertTrue((player.tossDie(dice, isMoving, board) >= 2) && (player.tossDie(dice, isMoving, board) <= 12));
-
-    }
-
 }
