3384505df37a9bd74739f4db79d64e43ffd5af9b
Furkan KUSE <furkankuse99@gmail.com>
2019-11-25 22:25:06 +0300

    Purchasable interface added, property and utility squares implemented by it.

---
+++ src/main/java/Square/Purchasable.java
@@ -0,0 +1,17 @@
+package Square;
+
+import Player.Player;
+
+public interface Purchasable {
+
+    int landValue = 0;
+    int rent = 0;
+    Player owner = null;
+
+    int getLandValue();
+    void setLandValue(int landValue);
+    int getRent();
+    void setRent(int rent);
+    Player getOwner();
+    void setOwner(Player owner);
+}
